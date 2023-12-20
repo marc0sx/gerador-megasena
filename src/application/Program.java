2e1bb07bf204e9e0ecd55f542960996ccb295d8c
Marcos Antônio <juniorswinchester@hotmail.com>
2023-12-20 20:23:55 -0300

    inicio

---
+++ src/application/Program.java
@@ -0,0 +1,140 @@
+package application;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Gerar numeros de quais jogos?");
+        System.out.println("1 - Mega Sena");
+        System.out.println("2 - Lotofácil");
+        int n = sc.nextInt();
+
+        if (n == 1) {
+            generateMegaSenaNumbers();
+        } else if (n == 2){
+        	generateLotofacilNumbers();
+        } else {
+        	System.out.println("Número invalido");
+        	
+        }sc.close();
+    }
+
+    private static void generateMegaSenaNumbers() {
+        int[] numeros = new int[6];
+        Random random = new Random();
+
+        for (int i = 0; i < 6; i++) {
+            int randomNumber;
+            do {
+                randomNumber = random.nextInt(60) + 1; 
+            } while (contains(numeros, randomNumber));
+
+            numeros[i] = randomNumber;
+        }
+
+        
+        Arrays.sort(numeros);
+
+        System.out.println("Números gerados para Mega Sena: " + Arrays.toString(numeros));
+    }
+
+    private static void generateLotofacilNumbers() {
+        int[] numeros = new int[15];
+        Random random = new Random();
+
+        for (int i = 0; i < 15; i++) {
+            int randomNumber;
+            do {
+                randomNumber = random.nextInt(25) + 1; // Gerar um número entre 1 e 60
+            } while (contains(numeros, randomNumber));
+
+            numeros[i] = randomNumber;
+        }
+
+        // Classificar os numeros em ordem crescente
+        Arrays.sort(numeros);
+
+        System.out.println("Números gerados para Lotofácil: " + Arrays.toString(numeros));
+    }
+    
+    private static boolean contains(int[] array, int number) {
+        for (int value : array) {
+            if (value == number) {
+                return true;
+            }
+        }
+        return false;
+    } 
+}
+
+
+
+
+
+
+
+
+
+/*
+ *         boolean validOption = false;
+
+        while (!validOption) {
+            System.out.println("Gerar números de quais jogos?");
+            System.out.println("1 - Mega Sena");
+            System.out.println("2 - Lotofácil");
+            System.out.println("0 - Sair");
+            int n = sc.nextInt();
+
+            switch (n) {
+                case 1:
+                    generateMegaSenaNumbers();
+                    validOption = true;
+                    break;
+                case 2:
+                    generateLotofacilNumbers();
+                    validOption = true;
+                    break;
+                case 0:
+                    System.out.println("Saindo do programa.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Número inválido. Por favor, escolha uma opção válida.");
+                    break;
+            }
+        }
+
+        sc.close();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+                System.out.println("Gerar numeros de quais jogos?");
+        System.out.println("1 - Mega Sena");
+        System.out.println("2 - Lotofácil");
+        int n = sc.nextInt();
+
+        if (n == 1) {
+            generateMegaSenaNumbers();
+        } else if (n == 2){
+        	generateLotofacilNumbers();
+        } else {
+        	System.out.println("Número invalido");
+        	
+        }sc.close();
+ * */
+
